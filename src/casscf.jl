726018476fa35a6e850042843f7e24d5b832b6f5
Nils Schieschke <nils.schieschke@mail.de>
2021-01-06 19:17:40 +0100

    slight restruct and beginnings of casscf implementation

---
+++ src/casscf.jl
@@ -0,0 +1,30 @@
+module casscf
+
+  include("juci.jl")
+  include("rhf.jl")
+  
+  function run_casscf()
+    #setup calculation, get AO integrals, start orbitals (AOOps["C"])
+    sett = juci.set_config()
+    mol = juci.read_mol(sett["molfile"])
+    AOint,dims = juci.get_ints(mol,sett)
+    AOops = rhf.core(mol,sett,AOint,dims)
+  
+    strl = gen_strl(sett)
+  
+  end #function casscf
+
+  function gen_strl(sett)
+  
+    nact = parse(Int,sett["nact"])
+    nel  = Int(parse(Int,sett["nel"])/2)
+  
+    str =[1:1:nel;]
+
+    #build graph field (to get dimenstion and adress weights)
+  
+    #loop over all determinants, calculate <det|a^+_p a_q |det> to get couplings
+  
+  end #function gen_strl
+
+end #module casscf
--- src/rhf.jl
+++ src/rhf.jl
@@ -1,105 +1,115 @@
-include("juci.jl")
-
-using Printf
-using TensorOperations
-using LinearAlgebra
-
-function rhf()
-  sett = juci.set_config()
-  Ethr = 1.0 * 10^(-parse(Float64,sett["econv"]))
-  Dthr = 1.0 * 10^(-parse(Float64,sett["denconv"]))
-  dampfac = parse(Float64,sett["hfdampfac"])
-  dampstp = parse(Float64,sett["hfdampstp"])
-  dampend = parse(Float64,sett["hfdampend"])
-
-  mol = juci.read_mol(sett["molfile"])
+module rhf
+  include("juci.jl")
   
-  AOint,dims = juci.get_ints(mol,sett)
-  AOops = build_AOops(AOint)
-
-  #hcore guess
-  P = calc_P(AOops,AOint,dims)
-  Pold = zeros(dims["nao"],dims["nao"])
-  Eguess = 0.5 * sum( P .* (AOops["H"] .+ AOops["F"] ) ) + mol.Vnuc
-  println("Hcore guess energy: ",Eguess)
-
-  E = zeros(Float64,0)
-  append!(E,Eguess)
-
-  @printf("\n")
-  @printf("HF It.  |  HF-Energy | abs(dE)   | abs(dD)  | dampfac\n")
-  @printf("-----------------------------------------\n")
-  for ite = 2:parse(Int,sett["hfmaxit"])+1
-
-    push!(AOops,"Fold" => AOops["F"])
-    #Build the Fock matrix
-    Cocc = AOops["C"][:,1:dims["nocc"]]
-    tstring=@elapsed @tensor begin
-      Jti[P]    := P[k,l]   * AOint["B"][P,k,l]
-      J[m,n]    := AOint["B"][P,m,n] * Jti[P]
-      K1[Q,m,c] := Cocc[r,c] * AOint["B"][Q,m,r]
-      K[m,n]    := K1[Q,m,p] * K1[Q,n,p]
-      F[m,n]    := AOops["H"][m,n] + J[m,n] - K[m,n]
-    end
-
-    #damping
-    F = dampfac * F .+ (1.0-dampfac) * AOops["Fold"]
-    dampfac = min(dampend,dampfac+dampstp)
-
-    push!(AOops,"F" => F)
-    Pold = P
-    P = calc_P(AOops,AOint,dims)
-    dD = P .- Pold
-    dP = sqrt(sum(dD.^2))
+  using Printf
+  using TensorOperations
+  using LinearAlgebra
+  
+  function run_rhf()
+    sett = juci.set_config()
+    mol = juci.read_mol(sett["molfile"])
+    AOint,dims = juci.get_ints(mol,sett)
+  
+    AOops = core(mol,sett,AOint,dims)
+  
+  end #function rhf
+  
+  function core(mol,sett,AOint,dims)
+    Ethr = 1.0 * 10^(-parse(Float64,sett["econv"]))
+    Dthr = 1.0 * 10^(-parse(Float64,sett["denconv"]))
+    dampfac = parse(Float64,sett["hfdampfac"])
+    dampstp = parse(Float64,sett["hfdampstp"])
+    dampend = parse(Float64,sett["hfdampend"])
     
-    append!(E,0.5 * sum(P .* ( AOops["H"] .+ AOops["F"] ) ) + mol.Vnuc)
-    dE = abs(E[ite]-E[ite-1])
-    @printf("  %3d   |  %.5f | %.2e | %.2e | %.1f\n",ite-1,E[ite],abs(dE),dP,dampfac)
-
-    if ( (abs(dE) < Ethr) & (dP < Dthr) & (ite > 5) )
-      @printf("HF is converged!\n")
-      break
-    end #if convergence
-     
-  end #ite
+    AOops = build_AOops(AOint)
   
-end #function rhf
-
-
-function build_AOops(AOint)
-  AOops = Dict()
-
-  #push!(AOops,"Sh" => AOint["S"]^(1/2))
-  push!(AOops,"H" => AOint["T"] .+ AOint["V"])
-  push!(AOops,"F" => AOops["H"])
-
-  #X matrix (canonical orthogonalization)
-  s,U = eigen(AOint["S"])
-  X = zeros(size(U,1),size(U,2))
-  for i in 1:size(s,1)
-    s[i] = 1.0/sqrt(s[i])
-  end
-  for i in 1:size(U,1)#, j in 1:size(U,2)
-    X[:,i] = U[:,i] * s[i]
-  end #i,j
-  push!(AOops,"X" => X)
-
-  return AOops
-end #function build_AOops
-
-
-function calc_P(AOops,AOint,dims)
-
-  Ft = transpose(AOops["X"])*AOops["F"]*AOops["X"]
-  eps,Ct = eigen(Ft)
+    #hcore guess
+    P = calc_P(AOops,AOint,dims)
+    Pold = zeros(dims["nao"],dims["nao"])
+    Eguess = 0.5 * sum( P .* (AOops["H"] .+ AOops["F"] ) ) + mol.Vnuc
+    println("Hcore guess energy: ",Eguess)
   
-  C = AOops["X"]*Ct
-  push!(AOops,"C" => C)
+    E = zeros(Float64,0)
+    append!(E,Eguess)
+  
+    @printf("\n")
+    @printf("HF It.  |  HF-Energy | abs(dE)   | abs(dD)  | dampfac\n")
+    @printf("-----------------------------------------\n")
+    for ite = 2:parse(Int,sett["hfmaxit"])+1
+  
+      push!(AOops,"Fold" => AOops["F"])
+      #Build the Fock matrix
+      Cocc = AOops["C"][:,1:dims["nocc"]]
+      tstring=@elapsed @tensor begin
+        Jti[P]    := P[k,l]   * AOint["B"][P,k,l]
+        J[m,n]    := AOint["B"][P,m,n] * Jti[P]
+        K1[Q,m,c] := Cocc[r,c] * AOint["B"][Q,m,r]
+        K[m,n]    := K1[Q,m,p] * K1[Q,n,p]
+        F[m,n]    := AOops["H"][m,n] + J[m,n] - K[m,n]
+      end
+  
+      #damping
+      F = dampfac * F .+ (1.0-dampfac) * AOops["Fold"]
+      dampfac = min(dampend,dampfac+dampstp)
+  
+      push!(AOops,"F" => F)
+      Pold = P
+      P = calc_P(AOops,AOint,dims)
+      dD = P .- Pold
+      dP = sqrt(sum(dD.^2))
+      
+      append!(E,0.5 * sum(P .* ( AOops["H"] .+ AOops["F"] ) ) + mol.Vnuc)
+      dE = abs(E[ite]-E[ite-1])
+      @printf("  %3d   |  %.5f | %.2e | %.2e | %.1f\n",ite-1,E[ite],abs(dE),dP,dampfac)
+  
+      if ( (abs(dE) < Ethr) & (dP < Dthr) & (ite > 5) )
+        @printf("HF is converged!\n")
+        break
+      end #if convergence
+       
+    end #ite
+  
+    return AOops
+    
+  end #function rhf_core
+  
+  
+  function build_AOops(AOint)
+    AOops = Dict()
+  
+    #push!(AOops,"Sh" => AOint["S"]^(1/2))
+    push!(AOops,"H" => AOint["T"] .+ AOint["V"])
+    push!(AOops,"F" => AOops["H"])
+  
+    #X matrix (canonical orthogonalization)
+    s,U = eigen(AOint["S"])
+    X = zeros(size(U,1),size(U,2))
+    for i in 1:size(s,1)
+      s[i] = 1.0/sqrt(s[i])
+    end
+    for i in 1:size(U,1)#, j in 1:size(U,2)
+      X[:,i] = U[:,i] * s[i]
+    end #i,j
+    push!(AOops,"X" => X)
+  
+    return AOops
+  end #function build_AOops
+  
+  
+  function calc_P(AOops,AOint,dims)
+  
+    Ft = transpose(AOops["X"])*AOops["F"]*AOops["X"]
+    eps,Ct = eigen(Ft)
+    
+    C = AOops["X"]*Ct
+    push!(AOops,"C" => C)
+    push!(AOops,"eps" => eps)
+  
+    Cocc = C[:,1:dims["nocc"]]
+    P    = zeros(dims["nao"],dims["nao"])
+    @tensor P[m,n] = 2.0 * Cocc[m,p] * Cocc[n,p]
+  
+    return P
+  end #function core_guess
 
-  Cocc = C[:,1:dims["nocc"]]
-  P    = zeros(dims["nao"],dims["nao"])
-  @tensor P[m,n] = 2.0 * Cocc[m,p] * Cocc[n,p]
-
-  return P
-end #function core_guess
-
+end #module rhf
